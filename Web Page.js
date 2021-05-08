d516eec4a5d4c72c80eddb7a3271f76b88c006ab
ithemark <avinashstarkvis@gmail.com>
2021-05-08 12:47:03 +0530

    Added page 2

--- Web Page.js
+++ Web Page.js
@@ -1,2 +1,17 @@
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+let viewPortheight = window.innerHeight;
+
+let browserHeight = window.outerHeight;
+
+let screenHeight = screen.height;
+
+let availHeight = screen.availHeight;
+
+let height = availHeight-(browserHeight-viewPortheight);
+
+window.onload = function testing(){
+    document.getElementById("header2").style.marginTop = height+"px";
+    document.getElementById("circleDiv2").style.marginTop = (height+240)+"px";
+    document.getElementById("nameDiv2").style.marginTop = (height+440)+"px";
+    document.getElementById("designationDiv2").style.marginTop = (height+550)+"px";
+    document.getElementById("lifeSpanDiv2").style.marginTop = (height+640)+"px";
+}
