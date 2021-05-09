8566e8b63972bdd4e63dbf36f75b37d46b2e8ff9
ithemark <avinashstarkvis@gmail.com>
2021-05-09 11:19:15 +0530

    Fixed image stretch and shrink

--- Web Page.js
+++ Web Page.js
@@ -9,21 +9,41 @@ let availHeight = screen.availHeight;
 let height = availHeight-(browserHeight-viewPortheight);
 
 window.onload = function pageHeight(){
+
+    //fixing page height
     document.getElementById("page1").style.height = height+"px";
     document.getElementById("page2").style.marginTop = (height)+"px";
     document.getElementById("page2").style.height = (height)+"px";
+
+    //calculating header size
     document.getElementById("header1").style.height = Math.max(height*(0.272))+"px";
     document.getElementById("header2").style.height = Math.max(height*(0.272))+"px";
+
+    //calculating positioning of div for circle
     document.getElementById("circleDiv1").style.height = Math.max(height*(0.181))+"px";
     document.getElementById("circleDiv1").style.marginTop = Math.max(height*(0.218))+"px";
     document.getElementById("circleDiv2").style.height = Math.max(height*(0.181))+"px";
     document.getElementById("circleDiv2").style.marginTop = Math.max(height*(0.218))+"px";
+
+    //calculating width of outer-circle
     document.getElementById("circle1").style.width = Math.max(height*(0.181))+"px";
     document.getElementById("circle2").style.width = Math.max(height*(0.181))+"px";
+
+    //calculating image position within circle
+    document.getElementById("circle1").getElementsByTagName("img")[0].style.height = Math.max((height*(0.181)*0.9))+"px";
+    document.getElementById("circle1").getElementsByTagName("img")[0].style.width = Math.max((height*(0.181)*0.9))+"px";
+    document.getElementById("circle2").getElementsByTagName("img")[0].style.height = Math.max((height*(0.181)*0.9))+"px";
+    document.getElementById("circle2").getElementsByTagName("img")[0].style.width = Math.max((height*(0.181)*0.9))+"px";
+
+    //calculating size of div for name
     document.getElementById("nameDiv1").style.marginTop = Math.max(height*(0.454))+"px";
     document.getElementById("nameDiv2").style.marginTop = Math.max(height*(0.454))+"px";
+
+    //calculating size of div for designation
     document.getElementById("designationDiv1").style.marginTop = Math.max(height*(0.636))+"px";
     document.getElementById("designationDiv2").style.marginTop = Math.max(height*(0.636))+"px";
+
+    //calculating size of div for life span
     document.getElementById("lifeSpanDiv1").style.marginTop = Math.max(height*(0.818))+"px";
     document.getElementById("lifeSpanDiv2").style.marginTop = Math.max(height*(0.818))+"px";
 }
